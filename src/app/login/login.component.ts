d5531c1a6def331aa4fe5d0640552ac345e62186
Patrick Yienger <PatrickYienger@Patricks-MacBook-Pro.local>
2018-03-28 13:22:41 -0500

    Updated SignUp and made a basic login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink} from '@angular/router';
+import { Login } from './../domain/models/login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    public logins: Login[];
 
-  ngOnInit() {
-  }
+    public newLogin: Login;
 
+    public userCheck: boolean = false;
+
+    public passCheck: boolean = false;
+
+    public reset: boolean = false;
+
+    constructor() {}
+
+    ngOnInit() {
+      this.logins = [];
+      this.newLogin = {};
+    }
+
+    login() {
+      if (this.reset)
+      {
+        this.userCheck = false;
+        this.passCheck = false;
+      }
+      if (!this.newLogin.password)
+      {
+        this.passCheck = true;
+      }
+      if (!this.newLogin.username)
+      {
+        this.userCheck = true;
+      }
+      if (this.passCheck == true || this.userCheck == true)
+      {
+        this.reset = true;
+        this.newLogin = {};
+      }
+      else
+      {
+        this.logins.push(this.newLogin);
+        this.newLogin = {};
+      }
+    }
 }
