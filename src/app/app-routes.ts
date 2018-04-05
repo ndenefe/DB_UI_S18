03b8c5b94234fb2148165601dbd92056752d4ca9
pyienger <pyienger@smu.edu>
2018-04-04 20:16:07 -0500

    Update app-routes.ts

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,6 +2,9 @@ import { Routes, RouterLink } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import {LoginComponent} from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {UpelectionComponent} from './upelection/upelection.component';
+
 export const APP_ROUTES: Routes = [
     {path: 'signup', component: SignUpComponent},
     {path: 'login', component: LoginComponent},
