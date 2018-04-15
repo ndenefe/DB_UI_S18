eba3426ec15f935890c87c01f3146ba05d247883
Rrrrrrrrrainus <yuc@smu.edu>
2018-04-15 16:43:25 -0500

    session working

--- test/test/server.js
+++ test/test/server.js
@@ -37,111 +37,28 @@ server.route({
     }
 });
 
-/*
-//A new route to test connectivity to MySQL
-server.route({
-    method: 'GET',
-    path: '/nonPol',
-    handler: function (request, reply) {
-        console.log('Server processing a /nonPol request');
-
-        //Does a simple select, not from a table, but essentially just uses MySQL
-        //to add 1 + 1.
-        //function (error, results, fields){...} is a call-back function that the
-        //MySQL lib uses to send info back such as if there was an error, and/or the
-        //actual results.
-        connection.query('SELECT firstName, lastName, email FROM nonPolitician', function (error, results, fields) {
-            if (error)
-                throw error;
-            //Sends back to the client the value of 1 + 1
-            reply (results);
-
-            //for exemplar purposes, stores the returned value in a variable to be
-        });
-    }
-    
-    
-});
-server.route({
-    method: 'GET',
-    path: '/pol',
-    handler: function (request, reply) {
-        console.log('Server processing a /pol request');
-        connection.query('SELECT firstName, lastName, email FROM politicians', function (error, results, fields) {
-            if (error)
-                throw error;
-            reply (results);
-
-        });
-    }
-});
-
-server.route({
-    method: 'POST',
-    path: '/login/nonPol',
-    handler: function (request, reply) {
-        console.log('Server processing a /login request');
-
-        connection.query('SELECT * FROM `nonPolitician` WHERE `username` =? AND `password` = ?', [request.payload['username'],request.payload['password']],function (error, results, fields) {
-            if (error)
-                throw error;
-            else{
-		if(results.length==1){
-			reply('Hello '+results[0].firstName+' '+results[0].lastName);
-		}
-		else
-			reply('Cannot find account, try it again.');
-	    }
-
-        });
-    }
-});
-
-server.route({
-    method: 'POST',
-    path: '/login/pol',
-    handler: function (request, reply) {
-        console.log('Server processing a /login request');
-
-        connection.query('SELECT * FROM `politicians` WHERE `username` =? AND `password` = ?', [request.payload['username'],request.payload['password']],function (error, results, fields) {
-            if (error)
-                throw error;
-            else{
-		if(results.length==1){
-			reply('Hello '+results[0].firstName+' '+results[0].lastName);
-		}
-		else
-			reply('Cannot find account, try it again.');
-	    }
-
-        });
-    }
-});
-
 
 server.route({
     method: 'GET',
-    path: '/search/pol/{name}',
-    handler: function (request, reply) {
-        console.log('Server processing a /search request');
-        connection.query('SELECT firstName,lastName, email, picture, partyId, phone, website, platformId FROM `politicians` WHERE `firstName`=? OR `lastName`=?', [request.params.name,request.params.name], function (error, results, fields) {
-            if (error)
-                throw error;
-            reply (results);
-
-        });
-    }
-});
-server.route({
-    method: 'GET',
-    path: '/search/nonPol/{name}',
+    path: '/search/{name}',
     handler: function (request, reply) {
         console.log('Server processing a /search request');
         connection.query('SELECT firstName,lastName, email, picture, phone  FROM `nonPolitician` WHERE `firstName`=? OR `lastName`=?', [request.params.name,request.params.name], function (error, results, fields) {
             if (error)
                 throw error;
-            reply (results);
+            else{
+                connection.query('SELECT firstName,lastName, email, picture, partyId, phone, website, platformId FROM `politicians` WHERE `firstName`=? OR `lastName`=?', [request.params.name,request.params.name], function (error, results2, fields) {
+                    if (error)
+                        throw error;
+                    else{
+                        if(results)
+                            reply('politician results: '+results);
+                        else if(results2)
+                            reply('nonpolitician results: '+results);
+                    }
 
+                });
+            }
         });
     }
 });
@@ -168,7 +85,7 @@ server.route({
         connection.query('SELECT firstName,lastName, email, picture, partyId, phone, website, dateTime, city,state FROM `politicians` NATURAL JOIN `candidates` NATURAL JOIN `elections` WHERE `electionId`=? AND (`firstName`=? OR `lastName`=?)', [request.params.election,request.params.name,request.params.name], function (error, results, fields) {
             if (error)
                 throw error;
-            if(results.length==1)
+            if(results)
                 reply (results);
             else
                 reply("The people you are looking for doesn't enroll this election. ");
@@ -193,14 +110,17 @@ server.route({
         });
     }
 });
-*/
+
 server.route({
     method: ['POST','GET'],
     path: '/login',
     handler: function (request, reply) {
         let cookie =request.state.session;
         console.log('Server processing a /login request');
-
+        if(cookie){
+            cookie.lastVisit=Date.now();
+            return reply.redirect('/login/userpage').state('session',cookie);
+        }
         connection.query('SELECT * FROM `politicians` WHERE `username` =? AND `password` = ? ', [request.payload['username'],request.payload['password']],function (error, results, fields) {
             if (error)
                 throw error;
@@ -209,23 +129,28 @@ server.route({
             if (error)
                 throw error;
             else{
-		if(results.length){
+		if(results.length==1){
                     var users=request.payload['username'];
                     var passs=request.payload['password'];
-                    cookie={
-                        username:users,
-                        password:passs
-                    };
+                    if(!cookie){
+                        cookie={
+                            username:users,
+                            password:passs
+                        };
+                    }
+                    
                     cookie.lastVisit=Date.now();
                     return reply.redirect('/login/userpage').state('session',cookie);
 		}
                 else if(results2.length==1){
                     var users=request.payload['username'];
                     var passs=request.payload['password'];
-                    cookie={
-                        username:users,
-                        password:passs
-                    };
+                    if(!cookie){
+                        cookie={
+                            username:users,
+                            password:passs
+                        };
+                    }
                     cookie.lastVisit=Date.now();
                     return reply.redirect('/login/userpage').state('session',cookie);
                     
@@ -329,41 +254,6 @@ server.route({
         }
 });
 
-server.route({
-    method: 'PUT',
-    path: '/login/profile/update/{user}/{pass}',
-    handler: function (request, reply) {
-        console.log('Server processing a /login and profile request');
-
-        connection.query('SELECT * FROM `politicians` WHERE `username` =? AND `password` = ? ', [request.params.user,request.params.pass],function (error, results, fields) {
-            if (error)
-                throw error;
-            
-            connection.query(' SELECT * FROM `nonPolitician` WHERE `username` =? AND `password` = ?', [request.params.user,request.params.pass],function (error, results2, fields) {
-            if (error)
-                throw error;
-            else{
-		if(results.length==1){
-			connection.query('UPDATE `politicians` SET `email`=?,`picture`=?,`phone`=?,`website`=? WHERE `username` =? AND `password` = ?', [request.payload['email'],request.payload['piture'],request.payload['phone'],request.payload['website'],request.params.user,request.params.pass],function (error, results, fields) {
-                            if (error)
-                                throw error;
-                            reply('update successful!');
-                        });
-		}
-                else if(results2.length==1){
-			connection.query('UPDATE `nonPolitician` SET `email`=?,`picture`=?,`phone`=? WHERE `username` =? AND `password` = ?', [request.payload['email'],request.payload['piture'],request.payload['phone'],request.params.user,request.params.pass],function (error, results, fields) {
-                            if (error)
-                                throw error;
-                            reply('update successful!');
-                        });
-		}
-		else
-			reply('Cannot find account, try it again.');
-	    }
-        });
-        });
-    }
-});
 
 
 
